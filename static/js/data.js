3f56bcf56c61390123de71b07feeb5a252295c61
nickmccarty <27744625+nickmccarty@users.noreply.github.com>
2020-05-12 14:56:50 -0700

    Add files via upload

---
+++ static/js/data.js
@@ -0,0 +1,77 @@
+var data = [
+    {
+      "name": "Reading List",
+      "parent": "null",
+      "children": [
+        {
+            "name": "Statistics",
+            "parent": "Reading List",
+            "children": [
+              {
+                "name": "Think Stats",
+                "url": "https://greenteapress.com/wp/think-stats-2e/",
+                "parent": "Statistics"
+              },
+              {
+                "name": "Think Bayes",
+                "url": "https://greenteapress.com/wp/think-bayes/",
+                "parent": "Statistics"
+              },
+              {
+                "name": "Bayesian Methods for Hackers",
+                "url": "http://camdavidsonpilon.github.io/Probabilistic-Programming-and-Bayesian-Methods-for-Hackers/",
+                "parent": "Statistics"
+              }
+            ]
+          },
+          {
+            "name": "Git",
+            "parent": "Reading List",
+            "children": [
+              {
+                "name": "Pro Git",
+                "url": "https://git-scm.com/book/en/v2",
+                "parent": "Git"
+              },
+            ]
+          },
+          {
+            "name": "Data Science",
+            "parent": "Reading List",
+            "children": [
+              {
+                "name": "Data Science at the Command Line",
+                "url": "https://www.datascienceatthecommandline.com/",
+                "parent": "Data Science"
+              },
+              {
+                "name": "Python Data Science Handbook",
+                "url": "https://jakevdp.github.io/PythonDataScienceHandbook/",
+                "parent": "Data Science"
+              },
+              {
+                "name": "R for Data Science",
+                "url": "https://r4ds.had.co.nz/",
+                "parent": "Data Science"
+              }
+            ]
+          },
+          {
+            "name": "Machine Learning",
+            "parent": "Reading List",
+            "children": [
+              {
+                "name": "The Hundred-Page Machine Learning Book",
+                "url": "http://themlbook.com/wiki/doku.php",
+                "parent": "Machine Learning"
+              },
+              {
+                "name": "Machine Learning Engineering",
+                "url": "http://www.mlebook.com/wiki/doku.php",
+                "parent": "Machine Learning"
+              },
+            ]
+          },
+      ]
+    }
+  ];
